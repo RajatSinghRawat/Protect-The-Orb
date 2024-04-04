afd826e0d851fcaa35ac22d39e4716bfd8c9f6b9
Rajat Singh <rajats520@gmail.com>
2024-04-04 21:17:44 +0530

    Added the Shooting Mechanism for the Barrel

--- Assets/Scripts/BarrelController.cs
+++ Assets/Scripts/BarrelController.cs
@@ -2,23 +2,50 @@ using UnityEngine;
 
 public class BarrelController : MonoBehaviour
 {
-    public float rotationSpeed = 10f;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private GameObject projectilePrefab;
+    //[SerializeField] private float projectileSpeed = 10f;
+    [SerializeField] private KeyCode ButtonForShooting;
+    [SerializeField] private Transform spawnPosition;
+    // Time between shots in seconds
+    [SerializeField] private float shootRate = 0.5f;
+    [SerializeField] private float projectileSpeed;
+    private float shootTimer = 0f;
+    private Vector3 mousePosition;
+    
 
     void Update()
     {
-        BarrelRotation(); 
+        BarrelRotation();
+        GetPlayerInput();
+        shootTimer += Time.deltaTime;
     }
 
     private void BarrelRotation()
     {
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePosition);
         Vector2 direction = (mouseWorldPos - transform.position).normalized;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         // Create a rotation based on the calculated angle
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         // Rotate the object towards the mouse position with a smooth interpolation
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-    }    
+    }
+
+    private void Shoot()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, spawnPosition.position, transform.rotation);
+        projectileObject.GetComponent<Projectile>().SetprojectileSpeed(projectileSpeed);
+    }
+
+    private void GetPlayerInput()
+    {
+       mousePosition = Input.mousePosition;
+       if (Input.GetKeyDown(ButtonForShooting) && shootTimer >= shootRate)
+       {
+            Shoot();
+            shootTimer = 0f;
+       }            
+    }
 }
 
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private float projectileSpeed;
+
+    //settters
+    public void SetprojectileSpeed(float speed)
+    {
+        projectileSpeed = speed;
+    }
+
+    private Rigidbody2D projectileRigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectileRigidBody = GetComponent<Rigidbody2D>();
+        projectileRigidBody.velocity = transform.right * projectileSpeed; // Adjust direction based on player's facing direction
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
